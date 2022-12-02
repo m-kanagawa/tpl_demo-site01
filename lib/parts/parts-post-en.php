33300c109eb258848229baa330bce1040d1f1ad3
m-kanagawa <k.masahiro1980@gmail.com>
2022-12-02 10:46:37 +0900

    お知らせ機能の調整

---
+++ lib/parts/parts-post-en.php
@@ -0,0 +1,138 @@
+<section class="news-box">
+	<h2 class="">News</h2>
+	<div class="news-box__inner">
+		<ul class="news-box__tab">
+			<li class="tab is-active">ALL</li>
+			<li class="tab">News</li>
+			<li class="tab">Event</li>
+			<li class="tab">Menu1</li>
+			<li class="tab">Menu2</li>
+			<li class="tab">Menu3</li>
+		</ul>
+
+		<div class="news-box__panel">
+			
+			<div class="panel is-show">
+				<dl>
+					<?php
+						$query = new WP_Query();
+						$arg = array (
+							'posts_per_page' => 5, // 表示する件数
+							// すべて表示させる場合は必要のない設定
+							'category__in' => array( 1,2,3,4,5 ), // 表示したいカテゴリーのスラッグを指定
+							'lang' => 'en' //英語のみ出力
+						);
+						$posts = $query->query( $arg );
+						foreach ( $posts as $post ): // ループの開始
+						setup_postdata( $post ); // 記事データの取得
+						// 記事のカテゴリー情報を取得する
+						$cat = get_the_category();
+						$cat_name = $cat[0]->cat_name; // カテゴリー名
+						$cat_slug = $cat[0]->category_nicename; // カテゴリースラッグ
+					?>
+					<dt><?php echo get_the_date(); ?></dt>
+					<dd>
+						<a href="<?php echo home_url(); ?>/category/<?php echo $cat_slug ?>/" class="cat <?php echo $cat_slug ?>"><?php echo $cat_name; ?></a><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</dd>
+					<?php endforeach; wp_reset_postdata(); // 直前のクエリを復元する ?>
+				</dl>
+			</div><!-- panel -->
+
+			<div class="panel">
+				<?php
+					$query = new WP_Query();
+					$arg = array(
+						'posts_per_page' => 5, // 表示する件数
+						'category_name' => 'news', // 表示したいカテゴリーのスラッグを指定
+						'lang' => 'en' //英語のみ出力
+					);
+					$posts = $query->query( $arg );
+					if( $posts ): ?>
+				<dl>
+					<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+					<dt><?php echo get_the_date(); ?></dt>
+					<dd><a href="<?php echo home_url(); ?>/category/news/" class="cat news">News</a><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+					<?php endforeach; ?>
+					<?php endif; wp_reset_postdata(); ?>
+				</dl>
+			</div><!-- ./panel -->
+
+			<div class="panel">
+				<?php
+					$query = new WP_Query();
+					$arg = array(
+						'posts_per_page' => 5, // 表示する件数
+						'category_name' => 'event', // 表示したいカテゴリーのスラッグを指定
+						'lang' => 'en' //英語のみ出力
+					);
+					$posts = $query->query( $arg );
+					if( $posts ): ?>
+				<dl>
+					<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+					<dt><?php echo get_the_date(); ?></dt>
+					<dd><a href="<?php echo home_url(); ?>/category/event/" class="cat event">Event</a><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+					<?php endforeach; ?>
+					<?php endif; wp_reset_postdata(); ?>
+				</dl>
+			</div><!-- ./panel -->
+
+			<div class="panel">
+				<?php
+					$query = new WP_Query();
+					$arg = array(
+						'posts_per_page' => 5, // 表示する件数
+						'category_name' => 'menu1', // 表示したいカテゴリーのスラッグを指定
+						'lang' => 'en' //英語のみ出力
+					);
+					$posts = $query->query( $arg );
+					if( $posts ): ?>
+				<dl>
+					<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+					<dt><?php echo get_the_date(); ?></dt>
+					<dd><a href="<?php echo home_url(); ?>/category/event/" class="cat menu1">Menu1</a><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+					<?php endforeach; ?>
+					<?php endif; wp_reset_postdata(); ?>
+				</dl>
+			</div><!-- ./panel -->
+
+			<div class="panel">
+				<?php
+					$query = new WP_Query();
+					$arg = array(
+						'posts_per_page' => 5, // 表示する件数
+						'category_name' => 'menu2', // 表示したいカテゴリーのスラッグを指定
+						'lang' => 'en' //英語のみ出力
+					);
+					$posts = $query->query( $arg );
+					if( $posts ): ?>
+				<dl>
+					<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+					<dt><?php echo get_the_date(); ?></dt>
+					<dd><a href="<?php echo home_url(); ?>/category/event/" class="cat menu2">Menu2</a><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+					<?php endforeach; ?>
+					<?php endif; wp_reset_postdata(); ?>
+				</dl>
+			</div><!-- ./panel -->
+
+			<div class="panel">
+				<?php
+					$query = new WP_Query();
+					$arg = array(
+						'posts_per_page' => 5, // 表示する件数
+						'category_name' => 'menu3', // 表示したいカテゴリーのスラッグを指定
+						'lang' => 'en' //英語のみ出力
+					);
+					$posts = $query->query( $arg );
+					if( $posts ): ?>
+				<dl>
+					<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+					<dt><?php echo get_the_date(); ?></dt>
+					<dd><a href="<?php echo home_url(); ?>/category/event/" class="cat menu3">Menu3</a><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+					<?php endforeach; ?>
+					<?php endif; wp_reset_postdata(); ?>
+				</dl>
+			</div><!-- ./panel -->
+
+		</div>
+	</div>
+</section>
--- pages/page-top.php
+++ pages/page-top.php
@@ -7,4 +7,18 @@
   </section>
 <?php endwhile; endif; ?>
 
+<div class="lang"><?php echo do_shortcode( '[bogo]' ); ?></div>
+
+
+<?php $locale = get_locale(); /* 現在のロケールを取得 */ ?>
+<?php if( 'en_US' == $locale ): ?>
+	<!-- （英語用） -->
+	<?php get_template_part("lib/parts/parts-post-en"); ?>
+<?php else: ?>
+	<!-- （英語以外用、日本語と英語のみの場合は日本語用） -->
+	<?php get_template_part("lib/parts/parts-post"); ?>
+<?php endif; ?>
+
+
+
 <?php get_footer(); ?>
